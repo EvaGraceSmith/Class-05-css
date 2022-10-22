8f3c495903fa7405369f288a7494d2b8005f42c0
EvaGraceSmith <smithevagrace@gmail.com>
2022-10-22 13:15:04 -0500

    changed JS questions

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ console.log('Hello World');
 /*** this is a multipline 
 */
 /** Comment
-  * each new line has a staer
+  * each new line has a star
   * as you can see
  */
 
@@ -17,8 +17,8 @@ var UserNamer;
 // equal sign does not mean equals it is assigning the result of the prompt to the userName variable. It is an assingment operator.
 //lets prompt a user for their name when they come to our site.
 //method
-let userName = prompt('What is your name?');
-let nameConfirm = confirm('Is the really your name?');
+let userName = prompt('What is your cat\'s name?');
+let nameConfirm = confirm('Is that really the name of your cat?');
 
 console.log('What is our input for user name: ', userName);
 console.log('Did they confirm? ', nameConfirm);
@@ -26,13 +26,13 @@ let time = prompt('What hour is it in Military time?');
 console.log('User time is: ', time);
 let message;
 if(time <= 11){
- message = 'Good Morning';
+ message = 'Top O\' da Mornin\' to ya!';
 }
 else if (time > 12 && time <= 17){
- message = 'Good Afternoon';
+ message = 'G\'Day Mate';
 }
 else if (time > 18 && time < 24){
- message = 'Good Evening';
+ message = 'Nighty Night!';
 }
 else {
  message = 'Hello, I dont know your time.';
